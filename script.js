efc47c14a4978e057c95e7c6acbc3e2035570c21
Eli Anway <elianway@Elis-Mac-mini.home>
2021-06-03 13:31:03 -0400

    Updated checkWinner logic

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ const Player = (sign) => {
 const gameBoard = (() => {
   const board = ["","","","","","","","",""];
 
-  const getField = (index) => {
+  const getBlock = (index) => {
     if (index > board.length) return;
     return board[index];
   };
 
-  const setField = (index, sign) => {
+  const setBlock = (index, sign) => {
     if (index > board.length) return;
     board[index] = sign;
   };
@@ -29,6 +29,108 @@ const gameBoard = (() => {
     };
   };
 
-  return {getField, setField, reset};
+  return {getBlock, setBlock, reset};
 })();
 
+const displayController = (() => {
+  const blockElements = document.querySelectorAll(".block");
+  const messageElement = document.getElementById("message");
+  const restartButton = document.getElementById("restart-button");
+
+  blockElements.forEach((block) => 
+    block.addEventListener("click", (e) => {
+      if (gameController.getIsOver() || e.target.textContent !== "") return;
+      gameController.playRound(parseInt(e.target.dataset.index));
+      updateGameboard();
+    })
+  );
+
+  restartButton.addEventListener("click", (e) => {
+    gameBoard.reset();
+    gameController.reset();
+    updateGameboard();
+    setMessageElement("Player X's turn");
+  });
+
+  const updateGameboard = () => {
+    for (let i = 0; i < blockElements.length; i++) {
+      blockElements[i].textContent = gameBoard.getBlock(i);
+    }
+  };
+
+  const setResultMessage = (winner) => {
+    if (winner === "Draw") {
+      setMessageElement("It's a draw!");
+    } else {
+      setMessageElement(`Player ${winner} has won!`);
+    }
+  };
+
+  const setMessageElement = (message) => {
+    messageElement.textContent = message;
+  };
+
+  return {setResultMessage, setMessageElement};
+
+})();
+
+const gameController = (() => {
+    const playerX = Player("X");
+    const playerO = Player("O");
+    let round = 1;
+    let isOver = false;
+  
+    const playRound = (blockIndex) => {
+      gameBoard.setBlock(blockIndex, getCurrentPlayerSign());
+      if (checkWinner(blockIndex)) {
+        displayController.setResultMessage(getCurrentPlayerSign());
+        isOver = true;
+        return;
+      }
+      if (round === 9) {
+        displayController.setResultMessage("Draw");
+        isOver = true;
+        return;
+      }
+      round++;
+      displayController.setMessageElement(
+        `Player ${getCurrentPlayerSign()}'s turn`
+      );
+    };
+  
+    const getCurrentPlayerSign = () => {
+      return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
+    };
+  
+    const checkWinner = (fieldIndex) => {
+      const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+  
+      return winConditions
+        .filter((combination) => combination.includes(fieldIndex))
+        .some((possibleCombination) =>
+          possibleCombination.every(
+            (index) => gameBoard.getBlock(index) === getCurrentPlayerSign()
+          )
+        );
+    };
+  
+    const getIsOver = () => {
+      return isOver;
+    };
+  
+    const reset = () => {
+      round = 1;
+      isOver = false;
+    };
+  
+    return { playRound, getIsOver, reset };
+  })();
